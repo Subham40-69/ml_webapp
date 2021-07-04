6988c6d420f05a2600ae342eee7a2d37dacbcd39
Ranit <54316475+Ron-17@users.noreply.github.com>
2021-07-04 21:35:00 +0530

    Add files via upload
    new app file

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 import seaborn as sns
 import numpy as np 
+import plotly.express as px
 
 
 st.write("""
@@ -17,4 +18,12 @@ df1
 if(inp=="Relative plot"):
     rel=sns.relplot(x="TotalPositiveCases",y="Recovered",data=df1)
     st.pyplot(rel)
+elif(inp=="Bar Chart"):
+    bar=px.bar(df1,x="RegionName",y="Recovered",color="TotalPositiveCases")
+    st.plotly_chart(bar)
+else:
+    pie=px.pie(df1,values="Recovered",names="RegionName")
+    st.plotly_chart(pie)
+    # bar=sns.barplot(x="TotalPositiveCases",y="Recovered",data=df1)
+    # st.pyplot(bar)
 
